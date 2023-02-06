d1e4e58c01cfee0d4118afa3ddc06279c39ad7c7
Kelly Zhou <yinglinzhou0@gmail.com>
2023-02-06 14:19:54 -0500

    p03

--- my_hash_map/lib/p03_hash_set.rb
+++ my_hash_map/lib/p03_hash_set.rb
@@ -7,18 +7,31 @@ class HashSet
   end
 
   def insert(key)
+    if !include?(key)
+      self[key] << key
+      @count += 1
+      if count == num_buckets - 1
+        resize! 
+      end
+    end
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      self[key].delete(key) 
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +39,13 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @store = Array.new(old_store.length * 2) { Array.new }
+    @count = 0
+    old_store.each do |bucket|
+      bucket.each do |item|
+        insert(item)
+      end
+    end
   end
 end
